37fe4a1675521345571e19484b8f410bc7b735d2
Dimasbdd <91084191+Dimasbdd@users.noreply.github.com>
2022-04-06 20:54:13 -0300

    att 06/04

--- javascript.js
+++ javascript.js
@@ -1,22 +1,44 @@
-function openbox(){
-    var respostacodigo = document.getElementById("rescodigo");
-    if(respostacodigo.style.display === "block") {
-        respostacodigo.style.display = "none"
-    } else {
-        respostacodigo.style.display = "block";
+    function numeroaleatorio(){
+        return Math.floor(Math.random() * 999);
     }
+    var txtaleatorio = numeroaleatorio();
+    var txtaleatorio
+    function letraaleatoria(){
+        return Math.floor(Math.random()).charAt();
+    }
+    
+/*  teste de gerar letra aleatoria      falha
+    function letra(length) {
+    var resultado = ''; 
+    var letras = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+    var qtdletras = characters.length;
+    for ( var i = 0; i < length; i++ ) {
+      resultado += letras.charAt(Math.floor(Math.random() * 
+ qtdletras));
+   }
+   return resultado;
 }
-
-/* teste
-<script>
-    var button = document.documentElement ??? ("corpo");
-    button.addEventListener("click", function(){
-        var respostacodigo = document.getElementById("rescodigo");
+   var txtletraaleatoria = letras();
+*/
+    function openbox(){
+        var respostacodigo = document.getElementById("rescodigo");       
         if(respostacodigo.style.display === "block") {
-            respostacodigo.style.display = "none"
+            respostacodigo.style.display = "none";
         } else {
             respostacodigo.style.display = "block";
+            document.getElementById("all").textContent= "#"+ txtaleatorio + "LL";
         }
-    });
-</script>
-*/
+    }
+    
+    
+    /* teste de surgir div          funcionou melhor, dps não ksksksks
+        var button = document.documentElement ????????????? ("corpo");
+        button.addEventListener("click", function(){
+            var respostacodigo = document.getElementById("rescodigo");
+            if(respostacodigo.style.display === "block") {
+                respostacodigo.style.display = "none"
+            } else {
+                respostacodigo.style.display = "block";
+            }
+        });
+    */
